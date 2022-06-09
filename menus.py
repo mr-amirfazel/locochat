b715dbab1e54251970463e5806e70c57a988636c
amirfazel <amirfazel45@gmail.com>
2022-06-09 11:03:39 +0430

    added basic dah board menu

--- menus.py
+++ menus.py
@@ -1,5 +1,6 @@
 welcome = "cli_assets/welcome.txt"
 enter = "cli_assets/enterMenu.txt"
+dash_board = "cli_assets/dash_board.txt"
 
 
 def printer(file_name):
@@ -11,5 +12,10 @@ def printer(file_name):
 def welcome_text():
     printer(welcome)
 
+
 def enter_menu():
     printer(enter)
+
+
+def dash_board_menu():
+    printer(dash_board)
